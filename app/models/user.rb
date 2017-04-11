c91060fa45e16ad6822004198c7f990c601a0e48
越智 昭文 <akifumi.ochi@gmail.com>
2017-04-11 15:30:28 +0900

    画像の名前に問題があったため修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
         provider: auth.provider,
         uid:      auth.uid,
         email:    auth.info.email ||= "#{auth.uid}-#{auth.provider}@example.com",
-        image_url:   auth.info.image,
+        image_url: auth.info.image,
         password: Devise.friendly_token[0, 20]
     )
     user.skip_confirmation!
