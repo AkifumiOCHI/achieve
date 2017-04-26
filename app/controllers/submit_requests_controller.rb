d11d15371a4c0f8e19e557f765fdc069d20afee3
越智 昭文 <akifumi.ochi@gmail.com>
2017-04-26 18:15:17 +0900

    タスクフロー機能

--- app/controllers/submit_requests_controller.rb
+++ app/controllers/submit_requests_controller.rb
@@ -1,6 +1,6 @@
 class SubmitRequestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_submit_request, only: [:show, :edit, :update, :destroy]
+  before_action :set_submit_request, only: [:show, :edit, :update, :destroy, :approve, :reject]
 
   # GET /submit_requests
   # GET /submit_requests.json
@@ -10,23 +10,6 @@ class SubmitRequestsController < ApplicationController
 
   def inbox
     @submit_requests = SubmitRequest.where(request_user_id: current_user.id).where(status: 1) #自分に来ている依頼（＝担当者idが自分）のみ表示+status1で依頼中のもののみ表示
-  end
-
-  def approve
-    if @submit_request.update(submit_request_params)
-      @submit_request.task.update(charge_id: current_user.id)
-      redirect_to inbox_submit_requests_path, notice: '依頼を承認しました！'
-    else
-      redirect_to inbox_submit_requests_path, notice: '不具合が発生しました。もう一度操作を行ってください。'
-    end
-  end
-
-  def reject
-    if @submit_request.update(submit_request_params)
-      redirect_to inbox_submit_requests_path, notice: '依頼を却下しました！'
-    else
-      redirect_to inbox_submit_requests_path, notice: '不具合が発生しました。もう一度操作を行ってください。'
-    end
   end
 
   # GET /submit_requests/1
@@ -47,10 +30,9 @@ class SubmitRequestsController < ApplicationController
   # POST /submit_requests.json
   def create
     @submit_request = current_user.submit_requests.build(submit_request_params)
-
     respond_to do |format|
       if @submit_request.save
-        format.html { redirect_to @submit_request, notice: 'タスク依頼が送られました！' }
+        format.html { redirect_to @submit_request, notice: 'タスク依頼が送信されました！' }
         format.json { render :show, status: :created, location: @submit_request }
       else
         format.html { render :new }
@@ -64,7 +46,7 @@ class SubmitRequestsController < ApplicationController
   def update
     respond_to do |format|
       if @submit_request.update(submit_request_params)
-        format.html { redirect_to @submit_request, notice: 'Submit request was successfully updated.' }
+        format.html { redirect_to @submit_request, notice: 'タスク依頼が更新されました！' }
         format.json { render :show, status: :ok, location: @submit_request }
       else
         format.html { render :edit }
@@ -73,12 +55,29 @@ class SubmitRequestsController < ApplicationController
     end
   end
 
+  def approve
+    if @submit_request.update(submit_request_params)
+      @submit_request.task.update(charge_id: current_user.id)
+      redirect_to inbox_submit_requests_path, notice: '依頼を承認しました！'
+    else
+      redirect_to inbox_submit_requests_path, notice: '不具合が発生しました。もう一度操作を行ってください。'
+    end
+  end
+
+  def reject
+    if @submit_request.update(submit_request_params)
+      redirect_to inbox_submit_requests_path, notice: '依頼を却下しました！'
+    else
+      redirect_to inbox_submit_requests_path, notice: '不具合が発生しました。もう一度操作を行ってください。'
+    end
+  end
+
   # DELETE /submit_requests/1
   # DELETE /submit_requests/1.json
   def destroy
     @submit_request.destroy
     respond_to do |format|
-      format.html { redirect_to submit_requests_url, notice: 'Submit request was successfully destroyed.' }
+      format.html { redirect_to submit_requests_url, notice: '依頼されたタスクが削除されました。' }
       format.json { head :no_content }
     end
   end
@@ -95,4 +94,5 @@ class SubmitRequestsController < ApplicationController
     def submit_request_params
       params.require(:submit_request).permit(:user_id, :task_id, :message, :request_user_id, :status)
     end
+
 end
