f65ab7d009d47b239d5ac86ca8f6c33431d59a11
越智 昭文 <akifumi.ochi@gmail.com>
2017-06-07 16:33:15 +0900

    Replace false with true of config.consider_all_requests_local in config/environments/development.rb

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,7 +5,7 @@ class BlogsController < ApplicationController
   def index
     @blogs = Blog.all
     # binding.pry
-    raise
+    # raise
     respond_to do |format|
       format.html
       format.js
